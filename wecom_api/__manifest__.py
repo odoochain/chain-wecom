707676a3b3b6d73426f1ee5519cc8676f6f59fee
daocalendar <along008@gmail.com>
2022-07-31 05:14:38 +0800

    update wecom_api

--- wecom_api/__manifest__.py
+++ wecom_api/__manifest__.py
@@ -3,12 +3,12 @@
     "name": "WeCom API",
     "author": "RStudio",
     "website": "https://gitee.com/rainbowstudio/wecom",
-    "sequence": 602,
+    "sequence": 600,
     "installable": True,
-    "application": True,
+    "application": False,
     "auto_install": False,
-    "category": "WeCom/WeCom",
-    "version": "14.0.0.1",
+    "category": "WeCom Suites/Settings",
+    "version": "14.0.0.2",
     "summary": """
 WeCom Service-side API and Client-side API              
         """,
@@ -16,30 +16,34 @@ WeCom Service-side API and Client-side API
  WeCom Service-side API and Client-side API
  Reconstruction based on project "https://github.com/sbzhu/weworkapi_python"
         """,
-    "depends": ["wecom_hrm"],
+    "depends": [],
     "data": [
         "security/ir.model.access.csv",
-        "data/ir_cron_data.xml",
-        "data/service_api_list_data.xml",
+        "data/ir_config_parameter.xml",
         "data/wecom_server_api_error_data.xml",
-        "data/wecom_app_event_type_data.xml",
-        "views/assets_templates.xml",
-        "views/ir_cron_views.xml",
-        # "views/wecom_service_api_views.xml",
-        "views/wecom_service_api_list_views.xml",
-        "views/wecom_service_api_error_views.xml",
-        "views/wecom_apps_views.xml",
-        "views/wecom_app_callback_service_views.xml",
-        "views/wecom_app_config_views.xml",
-        "views/wecom_app_type_views.xml",
-        "views/wecom_app_subtype_views.xml",
-        "views/wecom_app_event_type_views.xml",
-        "views/res_config_settings_views.xml",
-        "views/menu.xml",
+        "data/service_api_list_data.xml",
+        # "data/ir_cron_data.xml",
+        # "views/ir_cron_views.xml",
     ],
-    "external_dependencies": {
-        "python": ["requests_toolbelt", "pandas", "pycryptodome"],
+    "assets": {
+        "web.assets_common": [
+            # JS
+            "https://res.wx.qq.com/open/js/jweixin-1.2.0.js",
+            "wecom_api/static/src/js/wxconfig.js",
+        ],
+        "web.assets_backend": [
+            # JS
+        ],
     },
-    "qweb": ["static/src/xml/*.xml",],
+    "external_dependencies": {
+        "python": [
+            "requests_toolbelt",
+            "pandas",
+            "pypandoc==1.7.0",
+            "pycryptodome",
+            "html2text",
+        ],
+    },
+    "license": "LGPL-3",
     # "post_init_hook": "post_init_hook",
 }
