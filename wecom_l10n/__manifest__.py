8e8a600b0e409f22bf445ba75a8161084ecea2b6
daocalendar <along008@gmail.com>
2022-07-31 06:18:18 +0800

    update l10n

--- wecom_l10n/__manifest__.py
+++ wecom_l10n/__manifest__.py
@@ -9,11 +9,12 @@
     "application": False,
     "auto_install": True,
     "category": "WeCom/WeCom",
-    "version": "14.0.0.1",
+    "version": "14.0.0.2",
     "summary": """
         
         """,
     "description": """
 
         """,
+    "license": "LGPL-3",
 }
