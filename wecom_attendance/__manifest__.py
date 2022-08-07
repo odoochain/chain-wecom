305155144198a98ecea4b75698307050e96f296b
daocalendar <along008@gmail.com>
2022-08-08 05:55:19 +0800

    update wecom_attendance

--- wecom_attendance/__manifest__.py
+++ wecom_attendance/__manifest__.py
@@ -16,7 +16,11 @@
     "description": """
 
         """,
-    "depends": ["wecom_contacts", "hrms_base", "hrms_attendance",],
+    "depends": [
+        "wecom_contacts",
+        "hrms_base",
+        "wecom_contacts_sync",
+        "hrms_attendance",],
     "data": [
         "security/ir.model.access.csv",
         "data/wecom_apps_data.xml",
