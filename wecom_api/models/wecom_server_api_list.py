a065cba05cf0d73826ae5b6ff452e8cfed5fe83b
彩虹工作室 <rain.wen@outlook.com>
2022-08-28 17:52:42 +0800

    添加基础模块

---
+++ wecom_api/models/wecom_server_api_list.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import re
+from odoo import api, fields, models, SUPERUSER_ID, _
+
+
+class WecomServerApiList(models.Model):
+    _name = "wecom.service_api_list"
+    _description = "Wecom Server API List"
+    _order = "sequence,type"
+
+    type = fields.Selection(
+        [
+            ("1", "Base"), # 基础
+            ("2", "Contacts"), # 通讯录，
+            ("3", "Customer contact"), # 客户联系
+            ("4", "Wechat customer service"), # 微信客服
+            ("5", "Identity authentication"), # 身份验证
+            ("6", "Application management"), # 应用管理
+            ("7", "Message push"), # 消息推送
+            ("8", "Media material"), # 媒体素材
+            ("9", "OA"), # OA
+            ("10", "Efficiency tools"), # 效率工具
+            ("11", "externalpay"), # 企业支付
+            ("12", "Corp Group"), # 企业互联
+            ("13", "Corp Chain"), # 企业链，上下游
+            ("14", "Session content archiving"), # 会话内容存档
+            ("15", "Electronic invoice"), # 电子发票
+            ("16", "School"),   # 家校沟通
+            ("17", "School Apps"), # 家校应用
+            ("18", "Report"),  # 政民沟通
+        ],
+        string="Api Type",
+        required=True,
+        default="GET",
+    )  
+
+    name = fields.Char("Request Name", required=True,)
+    function_name = fields.Char("Request Function Name", required=True, readonly=True,)
+
+    short_url = fields.Char("Request Short Url", required=True,)
+
+    request_type = fields.Selection(
+        [("GET", "GET"), ("POST", "POST"),],
+        string="Api Request Type",
+        required=True,
+        default="GET",
+    )
+    description = fields.Html(string="Description")
+    sequence = fields.Integer(default=0)
+
+    _sql_constraints = [
+        ("function_name_uniq", "unique (function_name)", "The function is unique !",),
+    ]
+
+    def get_server_api_call(self, function_name):
+        """
+        根据函数名称获取 企业微信API的路由和请求方式
+        :param function_name : 函数名称
+        :returns 企业微信API的路由和请求方式的集合
+        """
+        # ['/cgi-bin/gettoken', 'GET']
+        data = []
+        res = self.search([("function_name", "=", function_name)], limit=1,)
+        data.append(res.short_url)
+        data.append(res.request_type)
+        return data
---
+++ wecom_base/__manifest__.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "WeCom Base",
+    "author": "RStudio",
+    "sequence": 601,
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "category": "WeCom Suites/Settings",
+    "website": "https://gitee.com/rainbowstudio/wecom",
+    "version": "15.0.0.1",
+    "summary": """
+        
+        """,
+    "description": """
+
+        """,
+    "depends": ["base_setup", "wecom_l10n", "wecom_widget", "wecom_api"],
+    "data": [
+        "security/wecom_security.xml",
+        "security/ir.model.access.csv",        
+        "data/ir_module_category_data.xml",
+        "data/ir_config_parameter.xml",
+        "data/ir_cron_data.xml",
+        "data/wecom_app_type_data.xml",
+        "views/ir_cron_views.xml",
+        "views/res_config_settings_views.xml",
+        "views/res_company_views.xml",
+        "views/wecom_service_api_list_views.xml",
+        "views/wecom_service_api_error_views.xml",
+        "views/wecom_apps_views.xml",
+        "views/wecom_app_config_views.xml",
+        "views/wecom_app_callback_service_views.xml",
+        "views/wecom_app_event_type_views.xml",
+        "views/wecom_app_type_views.xml",
+        "views/wecom_app_subtype_views.xml",
+        "views/menu_views.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            # SCSSS
+            "wecom_base/static/src/scss/wecom_settings_navigation.scss",
+            # JS
+            "wecom_base/static/src/js/wecom_settings_navigation.js",            
+            "wecom_base/static/src/js/list_header_button.js",
+        ],
+        "web.assets_qweb": [
+            "wecom_base/static/src/xml/*.xml",
+        ],
+    },
+    "license": "LGPL-3",
+    "bootstrap": True,
+    # "post_init_hook": "_open_wecom_settings",
+}
