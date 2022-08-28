805db07a95a18c4f652351d6496c195b9b08f8d7
彩虹工作室 <rain.wen@outlook.com>
2022-08-28 17:44:16 +0800

    清空

--- wecom_api/api/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import wecom_abstract_api
-from . import wecom_server_api
-from . import wecom_message_api
--- wecom_message/models/mail_activity.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, exceptions, fields, models, _, Command
-
-
-class MailActivity(models.Model):
-    _inherit = "mail.activity"
-
-    # send_wecom_message = fields.Boolean("Send Wecom Message", default=True,)
