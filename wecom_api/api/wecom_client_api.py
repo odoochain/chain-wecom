93ed7110771904c94a86b4ac110a17b3d4ed9557
rock <8676422+rockybb@user.noreply.gitee.com>
2023-06-21 01:39:02 +0000

    update wecom_api/api/wecom_client_api.py. 增加获取企业微信扫码组件的应用配置 获取企业微信扫码组件的应用配置
    Signed-off-by: rock <8676422+rockybb@user.noreply.gitee.com>

--- wecom_api/api/wecom_client_api.py
+++ wecom_api/api/wecom_client_api.py
@@ -45,3 +45,36 @@ class WecomClientApi(models.TransientModel):
             "configs":configs,
         })
         return result
+
+    @api.model
+    def getBarcodeWXConfig(self, postDatas):
+        """
+        获取企业微信扫码组件的应用配置
+        """
+        configs = []
+        result = {}
+        for data in postDatas:
+            config = {}
+            company =  self.env['res.company'].sudo().search([('id','=',data['company_id'])])
+            # wecom_apps = self.env['wecom.apps'].sudo().search([('type','=','self'), ('subtype_ids.code','=','auth')], limit=1)
+            timestamp = int(time.time())
+            nonceStr = self.env["wecomapi.tools.security"].sudo().random_str(8)
+            jsapi_ticket = company.wecom_jsapi_ticket
+            signature = self.env["wecomapi.tools.security"].sudo().generate_jsapi_signature(jsapi_ticket,nonceStr,timestamp)
+
+            config.update({
+                "company_id":company.id,
+                "corpid":company.corpid,
+                # "agentid":wecom_apps.agentid,
+                "timestamp":timestamp,
+                "nonceStr":nonceStr,
+                "signature":signature,
+                "jsApiList":['scanQRCode'],
+            })
+            configs.append(config)
+        result.update({
+            "code":0,
+            "msg":"ok",
+            "configs":configs,
+        })
+        return result
