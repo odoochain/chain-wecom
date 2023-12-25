538de83cfc2a550a49c76c497288980145e98428
彩虹工作室 <rain.wen@outlook.com>
2023-12-25 16:50:20 +0800

    添加微信验证登录模块和登录关爱模式

---
+++ web_login_care_model/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    "name": "Login page care mode",
+    "author": "RStudio",
+    "category": "Website/Website",
+    "sequence": 21,
+    "summary": "Registration and filing with MIIT and Public Security Bureau",
+    "description": """The text is larger and clearer.
+Stronger colors for better recognition.
+The button is larger and easier to use.
+     """,
+    "website": "",
+    "version": "16.0.0.1",
+    "depends": [
+        "web",
+    ],
+    "installable": True,
+    "application": True,
+    "data": [],
+    "assets": {
+        "web.assets_frontend": [],
+    },
+    "license": "OEEL-1",
+}
