037ffddddb0b50c6a9197e035a671dd4c1bf59a4
彩虹工作室 <rain.wen@outlook.com>
2024-01-27 13:17:48 +0800

    待处理微信公众号的采购和销售页面

--- wechat_web/__manifest__.py
+++ wechat_web/__manifest__.py
@@ -21,7 +21,8 @@ Allow users to log in through WeChat.
         "views/wechat_official_accounts_menus_views.xml",
         "views/wechat_applications_views.xml",
         "views/res_config_settings_views.xml",
-        "views/webclient.xml",
+        "views/webclient_templates.xml",
+        "views/portal_templates.xml",
         "views/menu_views.xml",
     ],
     "bootstrap": True,  # 加载登录屏幕的翻译，
@@ -30,6 +31,7 @@ Allow users to log in through WeChat.
             "wechat_web/static/**/*.js",
             "wechat_web/static/**/*.xml",
             "wechat_web/static/**/*.scss",
+            "https://res.wx.qq.com/t/wx_fed/weui-source/res/2.6.11/weui.min.css",
         ],
     },
     "license": "Other proprietary",
