dff7ca61e38df4a7572a783a36c470cbecffebce
彩虹工作室 <rain.wen@outlook.com>
2024-02-27 10:03:20 +0800

    完成通过微信公众号向微信用户发送确认的采购订单消

--- wechat_message_purchase/models/purchase.py
+++ wechat_message_purchase/models/purchase.py
@@ -7,6 +7,11 @@ from datetime import datetime
 class PurchaseOrder(models.Model):
 	_inherit = "purchase.order"
 
+	partner_type = fields.Selection(related="partner_id.company_type", string="Vendor Type",store=True)
+	partner_contact = fields.Many2one('res.partner', string='Vendor Contact', domain="[('parent_id', '=', partner_id)]")
+
+
+
 	def action_rfq_send_by_wechat(self):
 		"""
 		通过微信向供应商发送采购订单
@@ -16,11 +21,18 @@ class PurchaseOrder(models.Model):
 		wechat_message_template = self.env.ref("wechat_message_purchase.wechat_message_template_edi_purchase_done", raise_if_not_found=False)
 
 		ctx = {
-			"default_model": "sale.order",
+			"default_model": "purchase.order",
 			"default_res_id": self.id,
-			"default_partner_ids": self.partner_id.ids,
 			"default_template_id": wechat_message_template.id if wechat_message_template else None,
 		}
+		if self.partner_type =="person":
+			ctx.update({
+				"default_partner_ids": self.partner_contact.ids,
+			})
+		else:
+			ctx.update({
+				"default_partner_ids": self.partner_contact.ids,
+			})
 		return {
 			"type": "ir.actions.act_window",
 			"view_mode": "form",
@@ -30,3 +42,36 @@ class PurchaseOrder(models.Model):
 			"target": "new",
 			"context": ctx,
 		}
+
+
+	def get_vendor_name(self):
+		"""
+		获取供应商名称
+		"""
+		if self.partner_type=="company":
+			return self.partner_id.name
+		else:
+			return self.partner_id.parent_id.name
+			# if self.partner_id.parent_id:
+			# 	return self.partner_id.parent_id.name
+			# else:
+			# 	return _("None")
+
+	def get_vendor_contact_name(self):
+		"""
+		获取供应商联系人名称
+		"""
+		if self.partner_type=="company":
+			return self.partner_contact.name
+		else:
+			return self.partner_id.name
+
+	def get_amount_total(self):
+		amount_total = format_amount(self.env, self.amount_total, self.currency_id)
+
+		if "," in amount_total:
+			amount_total = amount_total.replace(",","")
+		return amount_total
+
+	def get_sent_time(self):
+		return format_datetime(self.env, datetime.now(), dt_format="yyyy-MM-dd HH:mm:ss", lang_code=self.env.lang)
